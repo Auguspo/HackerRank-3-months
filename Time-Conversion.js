f50cc35e58c7e8a3ecef23c61dceb55c7c3e25dc
auguspo <augustpolet@gmail.com>
2022-09-15 13:23:37 -0300

    First week seventh exercise

--- Time-Conversion.js
+++ Time-Conversion.js
@@ -1,31 +1,31 @@
-// Time Convertion 
+// Time Convertion
 // Given a time in 12-hour AM/PM format, convert it to military (24-hour) time.
 
 // Note: - 12:00:00AM on a 12-hour clock is 00:00:00 on a 24-hour clock.
-// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock. 
+// - 12:00:00PM on a 12-hour clock is 12:00:00 on a 24-hour clock.
 
-'use strict';
+"use strict"
 
-const fs = require('fs');
+const fs = require("fs")
 
-process.stdin.resume();
-process.stdin.setEncoding('utf-8');
+process.stdin.resume()
+process.stdin.setEncoding("utf-8")
 
-let inputString = '';
-let currentLine = 0;
+let inputString = ""
+let currentLine = 0
 
-process.stdin.on('data', function(inputStdin) {
-    inputString += inputStdin;
-});
+process.stdin.on("data", function (inputStdin) {
+  inputString += inputStdin
+})
 
-process.stdin.on('end', function() {
-    inputString = inputString.split('\n');
+process.stdin.on("end", function () {
+  inputString = inputString.split("\n")
 
-    main();
-});
+  main()
+})
 
 function readLine() {
-    return inputString[currentLine++];
+  return inputString[currentLine++]
 }
 
 /*
@@ -36,25 +36,28 @@ function readLine() {
  */
 
 function timeConversion(s) {
-  const arr = s.slice(0,8).split(':');
+  const arr = s.slice(0, 8).split(":")
 
-  arr[0] = (s.indexOf('PM') > -1) ?
-          (arr[0] == 12 ? '12' : Number(arr[0]) + 12) :
-          (arr[0] == 12 ? '00' : arr[0]);
-          
-  return arr.join(':');
+  arr[0] =
+    s.indexOf("PM") > -1
+      ? arr[0] == 12
+        ? "12"
+        : Number(arr[0]) + 12
+      : arr[0] == 12
+      ? "00"
+      : arr[0]
+
+  return arr.join(":")
 }
 
-
-
 function main() {
-    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+  const ws = fs.createWriteStream(process.env.OUTPUT_PATH)
 
-    const s = readLine();
+  const s = readLine()
 
-    const result = timeConversion(s);
+  const result = timeConversion(s)
 
-    ws.write(result + '\n');
+  ws.write(result + "\n")
 
-    ws.end();
+  ws.end()
 }
