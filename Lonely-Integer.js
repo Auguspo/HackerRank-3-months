fc4ab70f7c0483b94a0bab4c6d205d8e3a3fd55e
auguspo <augustpolet@gmail.com>
2022-09-16 13:44:43 -0300

    Second week first exercise

---
+++ Lonely-Integer.js
@@ -0,0 +1,54 @@
+//Given an array of integers, where all elements but one occur twice, find the unique element. 
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'lonelyinteger' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts INTEGER_ARRAY a as parameter.
+ */
+
+function lonelyinteger(arr) {
+ return arr.filter((item)=> arr.indexOf(item) === arr.lastIndexOf(item));
+ //Se filtra buscando la posicion del item desde el principio y el final, si la posicion es exactamente la misma significa q no hubo conincidcencias
+ 
+
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const a = readLine().replace(/\s+$/g, '').split(' ').map(aTemp => parseInt(aTemp, 10));
+
+    const result = lonelyinteger(a);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
