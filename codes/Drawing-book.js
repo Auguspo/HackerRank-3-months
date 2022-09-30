a22915a8a1ffd3de6350b85501d40d26f2df5eee
auguspo <augustpolet@gmail.com>
2022-09-29 22:23:23 -0300

    Third week eighth exercise

---
+++ codes/Drawing-book.js
@@ -0,0 +1,60 @@
+//Complete the pageCount function in the editor below. It should return the minimum number of pages Brie must turn.
+    //n: the number of pages in the book
+    //p: the page number to turn to
+
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'pageCount' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER n
+ *  2. INTEGER p
+ */
+
+function pageCount(n, p) {
+  let frontFlip = Math.floor(p/2);
+  let backFlip = Math.floor((n/2)-frontFlip);
+  
+  return Math.min(frontFlip, backFlip);
+   
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine().trim(), 10);
+
+    const p = parseInt(readLine().trim(), 10);
+
+    const result = pageCount(n, p);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
